d367540411850e8b89701c5d95095e0e9474d759
Prevost <yanis.prevost@epitech.eu>
2025-03-19 10:37:02 +0100

    merge

--- src/Game/Menu/Menu.cpp
+++ src/Game/Menu/Menu.cpp
@@ -21,12 +21,12 @@ void Menu::modules(IRenderer *renderer)
 
 void Menu::draw_game(IRenderer *renderer)
 {
-    Pacman pacman;
+    // Pacman pacman;
 
     int screenWidth = renderer->getScreenWidth();
     int screenHeight = renderer->getScreenHeight();
 
     renderer->DrawText1(((screenWidth / 2) / 1.2) + 10, 0, "Bienvenue sur Arcade !"); // Title
-    pacman.draw_game(renderer);
+    // pacman.draw_game(renderer);
     // modules(renderer);
 }
