d7c4538eebef9a40eed9e3068c8382bc3f796751
xmarano <leo.gregori@epitech.eu>
2025-03-20 15:39:27 +0100

    [ADD](Parsing) : Library not found && Wrong library

--- src/main.cpp
+++ src/main.cpp
@@ -43,11 +43,24 @@ void run_arcade(IDisplayModule*& currentDisplay, DLLoader<IDisplayModule>& loade
     }
 }
 
+void Parsing(int ac, char **av)
+{
+    if (ac != 2)
+        throw ArcadeException("Usage: ./arcade ./lib/lib.so");
+
+    std::string temp_file_path = av[1];
+    if (!std::filesystem::exists(temp_file_path))
+        throw ArcadeException("Library not found");
+
+    std::string file_path = temp_file_path.substr(temp_file_path.find_last_of("/") + 1);
+    if (file_path != "arcade_ncurses.so" && file_path != "arcade_sfml.so" && file_path != "arcade_sdl2.so")
+        throw ArcadeException("Wrong library");
+}
+
 int main(int ac, char **av)
 {
     try {
-        if (ac != 2)
-            throw ArcadeException("Usage: ./arcade ./lib/libncurses.so");
+        Parsing(ac, av);
         DLLoader<IDisplayModule> loader;
         IGameModule* currentGame = new Menu(); // Lance le menu au démarrage
         IDisplayModule* currentDisplay = loader.getInstance(av[1]);
